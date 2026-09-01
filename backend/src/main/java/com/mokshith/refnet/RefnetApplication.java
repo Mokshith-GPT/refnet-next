4db912e18aaeb16876bed4abbdb4dc3ee797bdc6
MOKSHITH Y NANDA <codersy17mc@gmail.com>
2026-09-01 13:13:16 +0530

    feat: add Spring Boot application entrypoint

---
+++ backend/src/main/java/com/mokshith/refnet/RefnetApplication.java
@@ -0,0 +1,9 @@
+package com.mokshith.refnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefnetApplication {
+  public static void main(String[] args) { SpringApplication.run(RefnetApplication.class, args); }
+}
